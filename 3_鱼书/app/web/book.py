8ab9e4c3ba2377801a51a03bdf1f1b90c749f748
kids0cn <kids0cn@gmail.com>
2024-10-02 00:00:20 +0800

    实现douban api isbn和名称搜索 有一个positional 参数错误是因为我调用了cls，但是没有在前面@classmethod

--- 3_鱼书/app/web/book.py
+++ 3_鱼书/app/web/book.py
@@ -2,7 +2,7 @@
 Author: kids0cn kids0cn@gmail.com
 Date: 2024-10-01 17:13:43
 LastEditors: kids0cn kids0cn@gmail.com
-LastEditTime: 2024-10-01 19:30:42
+LastEditTime: 2024-10-01 23:45:59
 FilePath: /learnFlask/3_鱼书/app/web/book.py
 Description: 
 
@@ -19,12 +19,14 @@ from flask import current_app
 
 web = Blueprint('web',__name__)
 
-@web.route('/book/search/<q>/<page>')
-def search(q,page):
+@web.route('/book/search/<q>')
+def search(q):
+    current_app.logger.info('搜索关键字：%s' % q)
     isbn_or_key = is_isbn_or_key(q)
     if isbn_or_key == 'isbn':
         result = YuShuBook.search_by_isbn(q)
     else:
+        current_app.logger.info('调用关键词搜索：搜索关键字：%s' % q)
         result = YuShuBook.search_by_keyword(q)
     return jsonify(result)
     # 返回json数据,这个就相当于是一个api
--- 3_鱼书/yushu_book.py
+++ 3_鱼书/yushu_book.py
@@ -2,7 +2,7 @@
 Author: kids0cn kids0cn@gmail.com
 Date: 2024-10-01 16:53:12
 LastEditors: kids0cn kids0cn@gmail.com
-LastEditTime: 2024-10-01 20:34:47
+LastEditTime: 2024-10-01 23:55:32
 FilePath: /learnFlask/3_鱼书/yushu_book.py
 Description: 
 
@@ -15,7 +15,8 @@ from flask import current_app
 class YuShuBook:
     # isbn_url = 'http://t.yushu.im/v2/book/isbn/{}'
     # keyword_url = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
-    douban_book_api = 'https://frodo.douban.com/api/v2/book/isbn/'
+    douban_isbn_api = 'https://frodo.douban.com/api/v2/book/isbn/'
+    douban_keyword_api = 'https://frodo.douban.com/api/v2/search/weixin'
     api_key = '0ac44ae016490db2204ce0a042db2916'
    # https://frodo.douban.com/api/v2/book/isbn/9787532796397?apiKey=0ac44ae016490db2204ce0a042db2916
    # 
@@ -29,13 +30,26 @@ class YuShuBook:
         params = {
             'apiKey':cls.api_key,
         }
-        url = cls.douban_book_api + isbn
-        current_app.logger.info(url)    
-        result = HTTP.get(url,return_json=True,params=params,headers=headers)
+        # current_app.logger.info(url)    
+        result = HTTP.get(cls.douban_isbn_api+isbn,return_json=True,params=params,headers=headers)
         return result
-    def search_by_keyword(cls,keyword,count=15,start=0):
-        url = cls.keyword_url.format(keyword,count,start)
-        result = HTTP.get(url)
+    
+    @classmethod
+    def search_by_keyword(cls,q):
+        current_app.logger.info('keyword搜索,keyword: %s' % q)
+        headers = {
+            'Referer':'https://servicewechat.com/wx2f9b06c1de1ccfca/95/page-frame.html',
+            'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 18_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.52(0x18003425) NetType/WIFI Language/zh_CN',
+            'HOST':'frodo.douban.com' # HOST指定目标主机
+        }
+        params = {
+            'q':q,
+            'count':15,
+            'start':0,
+            'apiKey':cls.api_key,
+        }
+        
+        result = HTTP.get(cls.douban_keyword_api,return_json=True,params=params,headers=headers)
         return result
 
     
