972b9fe757c1588abb0f4d4d57f243a5170b01eb
kids0cn <kids0cn@gmail.com>
2024-10-10 16:07:32 +0800

    改造伪类yushubook

--- 3_鱼书/app/spider/yushu_book.py
+++ 3_鱼书/app/spider/yushu_book.py
@@ -2,7 +2,7 @@
 Author: kids0cn kids0cn@gmail.com
 Date: 2024-10-01 16:53:12
 LastEditors: kids0cn kids0cn@gmail.com
-LastEditTime: 2024-10-08 22:05:28
+LastEditTime: 2024-10-10 16:04:57
 FilePath: /learnFlask/3_鱼书/app/spider/yushu_book.py
 Description: 
 
@@ -12,7 +12,7 @@ Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
 from app.libs.myHttp import HTTP
 from flask import current_app
 
-class YuShuBook:
+class YuShuBook_old:
     # isbn_url = 'http://t.yushu.im/v2/book/isbn/{}'
     # keyword_url = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
     douban_isbn_api = 'https://frodo.douban.com/api/v2/book/isbn/'
@@ -69,4 +69,72 @@ class YuShuBook:
         result = HTTP.get(cls.douban_keyword_api,return_json=True,params=params,headers=headers)
         return result
 
+class YushuBook:
+    # 这个类之所以是伪类是因为把所有的方法数据都返回给了别人，自己没有存储数据
+    def __init__(self):
+        self.total = 0;
+        self.books = []
+
+    # isbn_url = 'http://t.yushu.im/v2/book/isbn/{}'
+    # keyword_url = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
+    douban_isbn_api = 'https://frodo.douban.com/api/v2/book/isbn/'
+    douban_keyword_api = 'https://frodo.douban.com/api/v2/search/weixin'
+    api_key = '0ac44ae016490db2204ce0a042db2916'
+
     
+    def search_by_isbn(self ,isbn):
+        headers = {
+            'User-Agent':'MicroMessenger/',
+            'Referer':'https://servicewechat.com/wx2f9b06c1de1ccfca/91/page-frame.html'
+            }
+        params = {
+            'apiKey':self.api_key,
+        }
+        # current_app.logger.info(url)    
+        result = HTTP.get(self.douban_isbn_api+isbn,return_json=True,params=params,headers=headers)
+        '''
+        # 先从数据库查一下isbn，有的话直接返回
+        # 没有的话，调用豆瓣api
+        # 返回结果
+        参考伪代码
+        book = query_book_by_isbn(isbn)
+        if book:
+            return book
+        else:
+            result = HTTP.get(cls.douban_isbn_api+isbn,return_json=True,params=params,headers=headers)
+            if result.get('status') == 'success':
+                book = result.get('book')
+                return book
+            else:
+                return None
+
+        '''
+        self.__fill_single(result)
+    
+    
+    def search_by_keyword(self,q):
+        current_app.logger.info('keyword搜索,keyword: %s' % q)
+        headers = {
+            'Referer':'https://servicewechat.com/wx2f9b06c1de1ccfca/95/page-frame.html',
+            'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 18_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.52(0x18003425) NetType/WIFI Language/zh_CN',
+            'HOST':'frodo.douban.com' # HOST指定目标主机
+        }
+        params = {
+            'q':q,
+            'count':15,
+            'start':0,
+            'apiKey':self.api_key,
+        }
+        
+        result = HTTP.get(self.douban_keyword_api,return_json=True,params=params,headers=headers)
+        self.__fill_collection(result)
+    
+    def __fill_single(self,data):
+        if data:
+            self.total = 1
+            self.books.append(data)
+
+    def __fill_collection(self,data):
+        if data:
+            self.total = data['total']
+            self.books = data['books']
--- 3_鱼书/app/view_models/book.py
+++ 3_鱼书/app/view_models/book.py
@@ -2,7 +2,7 @@
 Author: kids0cn kids0cn@gmail.com
 Date: 2024-10-08 21:55:17
 LastEditors: kids0cn kids0cn@gmail.com
-LastEditTime: 2024-10-08 22:10:21
+LastEditTime: 2024-10-10 15:19:47
 FilePath: /learnFlask/3_鱼书/app/view_models/book.py
 Description: 
 
@@ -36,6 +36,7 @@ class BookViewModel:
         return returned
     
     def cut_data(self,data):
+        # 这里需要重写和测试
         book =  {
             'title':data['title'],
             'publisher':data['publisher'],
@@ -46,3 +47,21 @@ class BookViewModel:
             'image':data['image']
         }
         return book
+
+class BookViewModel_chaneg:
+    '''
+    # 重写本类，因为上面的类是面向过程的，只是封装了
+    # 到了一个类里
+    # 类最终的是：描述特征（类变量）和行为（方法），比如上面的类，就没有描述特征，
+    即没有存储数据
+
+    如何判断自己的类写的符不符合面向对象方法，
+    看自己的方法是不是大量的使用了@classmethod和@staticmethod
+    如果大量使用，说明这个类是面向过程的，不是面向对象的
+    
+
+
+    '''
+
+    
+
